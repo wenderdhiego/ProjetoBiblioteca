35872c0157809479812756b15d29ef0a3d6cd9eb
wenderdhiego <wenderdhiego@hotmail.com>
2021-10-10 16:19:40 -0300

    cadastro de livro ja emprestado indisponivel

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -79,6 +79,16 @@ namespace Biblioteca.Models
             }
         }
 
+         public ICollection<Emprestimo> ListarDisponiveis()
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                //busca os livros onde o id não está entre os ids de livro em empréstimo
+                // utiliza uma subconsulta
+                return bc.Livros.Where(l =>  !(bc.Emprestimos.Where(e => e.Devolvido == false).Select(e => e.LivroId).Contains(l.Id))).ToList();
+            }
+        }
+
         public Emprestimo ObterPorId(int id)
         {
             using(BibliotecaContext bc = new BibliotecaContext())
